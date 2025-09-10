0c6dd6d658bab4856155937b50bec405d029f6b9
George M <georgemargineanu20@gmail.com>
2025-09-10 16:27:18 +0300

    added project id

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ from gather_all import run_adreal_pipeline, get_correct_period
 
 def access_secret(secret_id, version_id="latest"):
     client = secretmanager.SecretManagerServiceClient()
-    project_id = "YOUR_PROJECT_ID"
+    project_id = "ums-adreal-471711"  # your actual project id
     name = f"projects/{project_id}/secrets/{secret_id}/versions/{version_id}"
     response = client.access_secret_version(name=name)
     return response.payload.data.decode("UTF-8")
