4feaf77926338916675e0d0af0d72a3d6aec46c9
George M <georgemargineanu20@gmail.com>
2025-09-15 12:32:11 +0300

    try no 1 remove Product

--- Muller/main.py
+++ Muller/main.py
@@ -40,7 +40,6 @@ def push_to_bigquery(df):
     df["Date"] = pd.to_datetime(df["Date"], errors="coerce").dt.date
     df["BrandOwner"] = df["BrandOwner"].astype(str)
     df["Brand"] = df["Brand"].astype(str)
-    df["Product"] = df["Product"].astype(str)
     df["ContentType"] = df["ContentType"].astype(str)
     df["MediaChannel"] = df["MediaChannel"].astype(str)
     df["AdContacts"] = pd.to_numeric(df.get("AdContacts"), errors="coerce").fillna(0).astype(int)
