38aa736c3dd8c3715b82df8e475fc24627b4988d
George M <georgemargineanu20@gmail.com>
2025-10-28 15:57:53 +0200

    Added CandyHaier

---
+++ CandyHaier/common/run_pipeline.py
@@ -0,0 +1,24 @@
+from gather_all import run_adreal_pipeline, get_correct_period
+from google.cloud import secretmanager
+
+def access_secret(secret_id, version_id="latest"):
+    client = secretmanager.SecretManagerServiceClient()
+    project_id = "ums-adreal-471711"  # your actual project id
+    name = f"projects/{project_id}/secrets/{secret_id}/versions/{version_id}"
+    response = client.access_secret_version(name=name)
+    return response.payload.data.decode("UTF-8")
+
+def main():
+    username = access_secret("adreal-username")
+    password = access_secret("adreal-password")
+    # CandyHaier Competitors
+    parent_brand_ids = ["947", "1551", "1056", "5297", "1709", "12988", "15875", "1248", "15652", "76815", "2126", "17574"]
+
+    df = run_adreal_pipeline(username, password, parent_brand_ids=parent_brand_ids)
+    print(f"Data fetched: {len(df)} rows")
+
+    period = get_correct_period()
+    df.to_csv(f"{period}_Adreal.csv", index=False)
+
+if __name__ == "__main__":
+    main()
