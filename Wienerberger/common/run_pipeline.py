418b4b7e12c464d250d7d68a7aff36cbaf511e35
George M <georgemargineanu20@gmail.com>
2025-11-04 17:26:13 +0200

    added wienerberger

---
+++ Wienerberger/common/run_pipeline.py
@@ -0,0 +1,25 @@
+from gather_all import run_adreal_pipeline, get_correct_period
+from google.cloud import secretmanager
+
+def access_secret(secret_id, version_id="latest"):
+    client = secretmanager.SecretManagerServiceClient()
+    project_id = "ums-adreal-471711"  # your actual project id
+    name = f"projects/{project_id}/secrets/{secret_id}/versions/{version_id}"
+    response = client.access_secret_version(name=name)
+    return response.payload.data.decode("UTF-8")
+
+def main():
+    username = access_secret("adreal-username")
+    password = access_secret("adreal-password")
+    # WienerBerger Competitors
+    parent_brand_ids = ["36509", "20217", "21069", "65043", "28621", "21505", "93174", "14387", "21317", "58218", "31820", 
+                            "59328", "37811", "24348", "21445", "25456", "51584", "52053"]
+    
+    df = run_adreal_pipeline(username, password, parent_brand_ids=parent_brand_ids)
+    print(f"Data fetched: {len(df)} rows")
+
+    period = get_correct_period()
+    df.to_csv(f"{period}_Adreal.csv", index=False)
+
+if __name__ == "__main__":
+    main()
